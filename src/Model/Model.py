c6e80778e61fdf1d5cae9d542068a882a85a431e
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2021-09-09 15:37:03 -0300

    increased step in inputExample for plotting

--- src/Model/Model.py
+++ src/Model/Model.py
@@ -22,7 +22,7 @@ class Model():
             plt.title(str(self))
             plt.scatter(self.inputDataSet[self.inputVariablesNames()], self.outputDataSet)
 
-            exampleInput = np.arange(self.inputDataSet.min()[0], self.inputDataSet.max()[0], 1)
+            exampleInput = np.arange(self.inputDataSet.min()[0], self.inputDataSet.max()[0], 0.1)
             examplePrediction = self.predict({self.inputVariablesNames()[0]: list(exampleInput)})
             plt.plot(exampleInput, examplePrediction, color='red')
 
